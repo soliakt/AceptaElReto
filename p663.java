b8b45777de51f3ffcc6ba889f0d42827e420fa0a
Victor Gimeno <soliakt4@gmail.com>
2023-04-01 22:48:33 +0200

    Contando desde el cero

--- p663.java
+++ p663.java
@@ -4,8 +4,8 @@ public class p663{
     public static void main(String[] args) {
         Scanner tec = new Scanner (System.in);
         int p, n = tec.nextInt();
-        for(int i=n; i>0; i--){
-            p=tec.nextInt();
+        for(int i = n; i > 0; i--){
+            p = tec.nextInt();
             if (p < 0) System.out.println(p);
             else System.out.println(p - 1);
         }
